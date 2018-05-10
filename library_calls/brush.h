703d6ba8d51ec9fb36e383aceae8b80ada70de1c
Lucca Zenobio <themrzmaster@gmail.com>
2018-05-10 15:22:47 -0300

    One more API

--- library_calls/brush.h
+++ library_calls/brush.h
@@ -18,9 +18,30 @@ VOID BH_GetSysColorBrush() {
 	logger("result_BH_GetSysColorBrush%s = GetSysColorBrush(tempInt_BH_GetSysColorBrush%s);", vid, vid);
 	result_BH_GetSysColorBrush = GetSysColorBrush(tempInt_BH_GetSysColorBrush);
 
-	logger("put_random_HANDLE(result_BH_GetSysColorBrush%s, TEXT(\"BH_GetSysColorBrush\"));", vid);
-	put_random_HANDLE(result_BH_GetSysColorBrush, TEXT("BH_GetSysColorBrush"));
+	//logger("put_random_HANDLE(result_BH_GetSysColorBrush%s, TEXT(\"BH_GetSysColorBrush\"));", vid);
+	//put_random_HANDLE(result_BH_GetSysColorBrush, TEXT("BH_GetSysColorBrush"));
 }
+
+
+VOID BH_CreateSolidBrush() {
+
+	HBRUSH result_BH_CreateSolidBrush;
+	COLORREF COLORREF_BH_CreateSolidBrush;
+
+	char vid[16];
+	sprintf(vid, "%d%d", get_time_in_ms(), rand() % 1024);
+	logger("HBRUSH result_BH_CreateSolidBrush%s;", vid);
+
+	COLORREF_BH_CreateSolidBrush = get_COLORREF(vid);
+
+	logger("//[Library_Call]: BH_CreateSolidBrush");
+	logger("result_BH_CreateSolidBrush = CreateSolidBrush(COLORREF_BH_CreateSolidBrush%s);", vid);
+	result_BH_CreateSolidBrush = CreateSolidBrush(COLORREF_BH_CreateSolidBrush);
+
+	//logger("put_random_HANDLE(result_BH_CreateSolidBrush%s, TEXT(\"BH_CreateSolidBrush\"));", vid);
+	//put_random_HANDLE(result_BH_CreateSolidBrush, TEXT("BH_CreateSolidBrush"));
+}
+
 
 
 
