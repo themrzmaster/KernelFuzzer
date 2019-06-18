0e234d998a78f2d4fe42019e5cdbfd02d342ae75
Lucca Zenobio <themrzmaster@gmail.com>
2019-06-18 19:52:13 -0300

    Use Visual Studio 2017 Compiler

--- bughunt_build_x64_debug.bat
+++ bughunt_build_x64_debug.bat
@@ -2,7 +2,7 @@
 echo:
 echo [Build] Setting up environment variables...
 echo:
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 echo:
 echo [Build] Removing old files...
 echo:
@@ -17,5 +17,5 @@ ml64.exe /c /Cx bughunt_syscall_x64.asm
 echo:
 echo [Build] Final compilation and linking...
 echo:
-cl.exe /Zi /Tc bughunt.c gdi32.lib Kernel32.lib User32.lib Advapi32.lib Shell32.lib Msimg32.lib Dxva2.lib Mscms.lib bughunt_syscall_x64.obj
+cl.exe /Zi  /Tc  bughunt.c gdi32.lib bcrypt.lib Kernel32.lib User32.lib Advapi32.lib Shell32.lib Msimg32.lib Dxva2.lib Mscms.lib bughunt_syscall_x64.obj
 ::cl.exe /Zi /Tc reproduce/repro.c gdi32.lib kernel32.lib User32.lib Advapi32.lib Shell32.lib Msimg32.lib Dxva2.lib Mscms.lib bughunt_syscall_x64.obj
